be3fdd9a3a70975db09248ac508e42148f2a5ebe
michaelmcconkey <mimc5594@colorado.edu>
2017-03-05 17:22:22 +0000

    Updated database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '2001:4860:4864:1:5423:42ea:2861:342e',
+	'hostname' => 'mcconkey-sql',
 	'username' => 'cinematweet',
 	'password' => '51171eXf!',
 	'database' => 'cinematweet',
